aeabd1412433ea633ee58228912372d842878e39
xuk <xuk@133.cn>
2019-01-21 20:33:59 +0800

    添加旋转截图

--- src/main/java/tree/binary/search/BinaryTreeDisplay.java
+++ src/main/java/tree/binary/search/BinaryTreeDisplay.java
@@ -7,7 +7,7 @@ public class BinaryTreeDisplay extends JApplet {
     private static final long serialVersionUID = 5329743141395531185L;
 
     public BinaryTreeDisplay() {
-        Integer[] numbers = {5, 3, 7, 4, 2, 8, 6};
+        Integer[] numbers = {7, 5, 6, 3, 2, 4, 8};
         BinaryTree<Integer> intTree = new BinaryTree<>(numbers);
         System.out.println("树大小为：" + intTree.size + "，高度为：" + intTree.height());
         add(new BinaryTreeControl(intTree));
