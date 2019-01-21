88604d7dfeec1d935085044d7765ff617abdf05e
xukai8817 <595404502@qq.com>
2019-01-22 00:06:35 +0800

    红黑树插入图片完成

--- src/main/java/tree/binary/search/BinaryTreeDisplay.java
+++ src/main/java/tree/binary/search/BinaryTreeDisplay.java
@@ -7,7 +7,7 @@ public class BinaryTreeDisplay extends JApplet {
     private static final long serialVersionUID = 5329743141395531185L;
 
     public BinaryTreeDisplay() {
-        Integer[] numbers = {7, 5, 6, 3, 2, 4, 8};
+        Integer[] numbers = {5, 3, 7, 2, 4, 6, 7, 8};
         BinaryTree<Integer> intTree = new BinaryTree<>(numbers);
         System.out.println("树大小为：" + intTree.size + "，高度为：" + intTree.height());
         add(new BinaryTreeControl(intTree));
